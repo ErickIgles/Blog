97ac08e3fc761b03c1fe7c2a2b4fb90fa40e6396
ErickIgles <erick.almeida2125@gmail.com>
2025-06-25 17:44:33 -0300

    feat: adicionado modelo 'CreatePosts' no painel administrativo.

--- posts/admin.py
+++ posts/admin.py
@@ -1,2 +1,8 @@
 from django.contrib import admin
+from .models import CreatePosts
+
+@admin.register(CreatePosts)
+class CreatePostsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'post_content', 'slug','post_image', 'created_at', 'updated_at', 'active')
+    list_display_links = ('title', 'post_content')
 
