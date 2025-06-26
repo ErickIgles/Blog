cf5850bb67504b29a286cbcdad8de89d65b52f39
ErickIgles <erick.almeida2125@gmail.com>
2025-06-26 20:55:02 -0300

    feat: implementado busca de postagens via API com renderização dinâmica

---
+++ posts/static/posts/js/script.js
@@ -0,0 +1,30 @@
+const msg = document.getElementById('comprimento');
+msg.innerHTML = 'BLOG'
+
+
+async function buscarPosts() {
+    try {
+        const resposta = await fetch('http://127.0.0.1:8000/api/posts/v1/');
+        const posts = await resposta.json();
+
+        const containerPosts = document.getElementById('sessao-posts');
+        
+        console.log(posts);
+        
+        posts.forEach(post => {
+            const link = document.createElement('a');
+            link.href = `/posts/${post.id}/`;
+            link.classList.add('card');
+            
+            link.innerHTML = `<img src="${post.post_image}" alt="Imagem de ${post.title}">`
+
+            containerPosts.appendChild(link);
+        });
+
+    } catch (error) {
+        console.log('Erro ao buscar dados:', error);
+    }
+}
+
+buscarPosts();
+
