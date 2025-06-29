6f234c1bba8726b9dbe21defd1a97f262d07e26d
ErickIgles <erick.almeida2125@gmail.com>
2025-06-29 11:11:10 -0300

    feat: criado classes de view para renderização de templates de detalhe e atualização de postagens.

--- posts/views.py
+++ posts/views.py
@@ -6,3 +6,10 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = 'index.html'
 
+
+class DetailPostView(TemplateView):
+    template_name = 'detail.html'
+
+class UpdatePost(TemplateView):
+    template_name = 'update.html'
+
