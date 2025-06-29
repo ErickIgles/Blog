98f96d30918301ad40bc00bf17da7ad8bea1da1d
ErickIgles <erick.almeida2125@gmail.com>
2025-06-29 11:06:13 -0300

    feat: adicionado rotas de detalhe e atualização de postagens

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, DetailPostView, UpdatePost
 
 urlpatterns = [
     path('', IndexView.as_view(), name='posts'),
+    path('detail/<int:pk>/', DetailPostView.as_view(), name='post_detail'),
+    path('update/<int:pk>/', UpdatePost.as_view(), name='post_update'),
 ]
